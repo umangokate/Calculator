c7f4f27716efa7bde1d4d3a5b2fe7def4674dde6
umangokate <umangokate@gmail.com>
2022-05-03 16:11:27 +0530

    Added Comment

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+//Comment added
 public class Calculator {
 
     private static final Logger logger = LogManager.getLogger(Calculator.class);
