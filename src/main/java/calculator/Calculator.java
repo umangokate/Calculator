f7fc0b9a7c179786687d8a7aa21dcdbdddd346be
umangokate <umangokate@gmail.com>
2022-05-03 15:10:51 +0530

    comment deleted

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-//New comment
+
 public class Calculator {
 
     private static final Logger logger = LogManager.getLogger(Calculator.class);
